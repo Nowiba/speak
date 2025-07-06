c26fb41ccaf21673115d31428c2b4dfb384f4811
Nowiba <mohamedlabido2646@gmail.com>
2025-07-06 09:19:13 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBMJuAmIAqDUXhZcGBHq8CszhJL92VaZ64",
-  authDomain: "information-project1.firebaseapp.com",
-  databaseURL: "https://information-project1-default-rtdb.firebaseio.com",
-  projectId: "information-project1",
-  storageBucket: "information-project1.firebasestorage.app",
-  messagingSenderId: "1098270976013",
-  appId: "1:1098270976013:web:2e1ca7602b316b6ed30d44",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
